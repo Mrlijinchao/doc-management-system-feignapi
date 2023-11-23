2d275fab807157b78dd500e1fd618fc2bfde0317
Mrlijinchao <2116639781@qq.com>
2023-11-23 21:33:15 +0800

    init project

---
+++ src/main/java/com/lijinchao/DocManagementSystemFeignapiApplication.java
@@ -0,0 +1,13 @@
+package com.lijinchao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocManagementSystemFeignapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DocManagementSystemFeignapiApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/lijinchao/openFeignFacade/systemProjectFacade/ProjectUserFacade.java
@@ -0,0 +1,8 @@
+package com.lijinchao.openFeignFacade.systemProjectFacade;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+public interface ProjectUserFacade {
+
+}
